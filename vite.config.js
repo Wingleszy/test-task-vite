077fe1ccdf255144bc8b88b52f4847cabf380555
Илья <meowingless@mail.ru>
2024-07-01 23:52:04 +0300

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://wingleszy.github.io/test-task-vite',
   plugins: [react()],
   server: {
     port: 3000, 
